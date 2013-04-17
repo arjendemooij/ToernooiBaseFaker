c52561b7138cc7e957a8a3ca3d53000578f17307
Arjen de Mooij <arjendemooij@gmail.com>
2013-04-17 20:11:08 +0200

    big

---
+++ TestToernooiBaseFakerUsage/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    
+</asp:Content>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <%= GetToernooiBaseContent() %>
+</asp:Content>
---
+++ TestToernooiBaseFakerUsage/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    private const string ToernooiBaseUrlQueryParameter = "toernooiBaseUrl";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    public string GetToernooiBaseContent()
+    {
+        string relativeUrl = GetToernooiBaseRelativeUrl();
+        string localUrlFormat = GetLocalUrlFormat();
+
+        string remoteUrl = string.Format("http://localhost:4625/Home/ToernooiBasePage?relativeUrl={0}&linkFormat={1}", HttpUtility.UrlEncode(relativeUrl), HttpUtility.UrlEncode(localUrlFormat)); 
+        string html = new WebClient().DownloadString(remoteUrl);
+
+        return html;
+    }
+
+    private string GetLocalUrlFormat()
+    {
+        string thisRequestUrl = Request.Url.AbsoluteUri;
+        
+        if(RequestHasToernooiBaseParameterValue())
+        {
+            string parameterValueString = string.Format("{0}={1}", ToernooiBaseUrlQueryParameter, HttpUtility.UrlEncode(GetToernooiBaseRelativeUrlFromGet()));
+            return thisRequestUrl.Replace(parameterValueString, ToernooiBaseUrlQueryParameter + "={0}");
+        }
+        else
+        {
+            string seperator =  thisRequestUrl.Contains("?") ? "&": "?";
+            return thisRequestUrl + seperator + ToernooiBaseUrlQueryParameter + "={0}";
+        }
+        
+    }
+
+    private bool RequestHasToernooiBaseParameterValue()
+    {
+        return !string.IsNullOrEmpty(GetToernooiBaseRelativeUrlFromGet()); 
+    }
+
+    private string GetToernooiBaseRelativeUrl()
+    {
+        string relativeUrlFromGet = GetToernooiBaseRelativeUrlFromGet();
+
+        string defaultRelativeUrl = "/opvraag/standenke.php?taal=&Id=3094&jr=13";
+        string relativeUrl = !string.IsNullOrEmpty(relativeUrlFromGet)
+            ? relativeUrlFromGet
+            : defaultRelativeUrl;
+
+        return relativeUrl;
+    }
+
+    private string GetToernooiBaseRelativeUrlFromGet()
+    {
+        string relativeUrlFromGet = Request["toernooiBaseUrl"];
+        return relativeUrlFromGet;
+    }
+
+}
